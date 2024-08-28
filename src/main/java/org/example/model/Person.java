e1e54aa3f5ef52968fba6b88f39f9e843b5f46d5
y.terinc <y.terinc@gmail.com>
2024-08-28 09:16:00 +0700

    1. task 48 - finish

---
+++ src/main/java/org/example/model/Person.java
@@ -0,0 +1,51 @@
+package org.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(int age, String name, int id) {
+        this.age = age;
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

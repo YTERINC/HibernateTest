f28c2675faa64ae6a4d156ddac34bc69ce341370
y.terinc <y.terinc@gmail.com>
2024-08-29 19:10:39 +0700

    5. task 52 - Отношение Один ко Многим (One-to-Many)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,69 @@
 package org.example;
 
 
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
-
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
 
-//            List<Person> people = session.createQuery("from Person where age > 30").getResultList();
-//            List<Person> people = session.createQuery("from Person where name LIKE 'T%'").getResultList();
-//            session.createQuery("update Person set name='Test' where age < 40").executeUpdate();
-            session.createQuery("delete from Person where age < 40").executeUpdate();
-//            for (Person p : people) {
-//                System.out.println(p);
+//            Person person = session.get(Person.class, 3);
+//            System.out.println(person);
+//            List<Item> items = person.getItems();
+//            System.out.println(items.toString());
+
+//            Item item = session.get(Item.class, 5);
+//            System.out.println(item);
+//            Person person = item.getOwner();
+//            System.out.println(person);
+
+//            Person person = session.get(Person.class, 2);
+//            Item newItem = new Item("Item from Hibernate", person);
+//            person.getItems().add(newItem); // делать с двух сторон не обязательно, но лучше делать.
+//            // Это для объекта в кэше. В базе и так все сделано
+//            session.save(newItem);
+
+//            Person person = new Person("Test person", 30);
+//            Item newItem = new Item("Test from Hibernate 2", person);
+//            person.setItems(new ArrayList<>(Collections.singletonList(newItem)));
+//            session.save(person); // cохраняем все сущности, т.к. каскадиварования не настроено
+//            session.save(newItem);
+
+//            Person person = session.get(Person.class,3);
+//            List<Item> items = person.getItems();
+//            for (Item item : items) {
+//                session.remove(item);
 //            }
+//            // не пораждает SQL, но необходимо для кэша
+//            person.getItems().clear();
+
+//            Person person = session.get(Person.class,2);
+//            session.remove(person);
+//            // чтобы было правильное состояние кэша
+//            person.getItems().forEach(i -> i.setOwner(null));
+
+            Person person = session.get(Person.class, 3);
+            Item item = session.get(Item.class, 1);
+            item.getOwner().getItems().remove(item);
+            // SQL запрос
+            item.setOwner(person);
+            person.getItems().add(item);
 
             session.getTransaction().commit();
+
         } finally {
             sessionFactory.close();
         }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "Person")
@@ -9,17 +10,22 @@ public class Person {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "age")
     private int age;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
 
     public Person() {
     }
 
-    public Person(String name,int age) {
-        this.age = age;
+    public Person(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     public int getId() {
@@ -53,5 +59,13 @@ public class Person {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 }
